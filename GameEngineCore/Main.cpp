dcaf89cecb7e2b6e93dd5986bd37748ece14b3b5
Bilal Baeza <bilalbaeza@gmail.com>
2015-12-17 14:40:48 -0500

    Added Classes to Load Images
    This is the version that has the main game code disabled in a #if
    statement
    Note
    **THE ADDED CLASSES ARE NOT MINE. LOOK AT THE COMMENTS AT THE TOP OF THE
    CLASSES FOR OWNERSHIP**

--- GameEngineCore/Main.cpp
+++ GameEngineCore/Main.cpp
@@ -5,6 +5,8 @@
 #include "src\objects\Wall.h"
 #include "src\objects\BaseEnemy.h"
 
+#include "src\graphics\lodepng.h"
+
 using namespace spacey;
 using namespace graphics;
 using namespace objects;
@@ -44,6 +46,7 @@ using namespace motion;
 
 */
 
+#if 0
 int main(){
 	//Width and height for the game window
 	int width = 800;
@@ -154,4 +157,110 @@ int main(){
 	}
 	return 0;
 }
+#endif
 
+int main(){
+	int s_width = 800;
+	int s_height = 600;
+
+	//Start Screen
+	static Window start("Still Alive - Start", s_width, s_height);
+
+	/*button b_start(&start, 250, 400, 50, 40, "Brandenbrug Tor.jpg");
+	button b_exit(&start, 550, 400, 50, 40, "sample.bmp");*/
+	bool exit = false; //Exit bool having to do with the commented out code above (Start Window)
+
+	const char* filename = "ship.png";
+
+	// Load file and decode image.
+	std::vector<unsigned char> image;
+	unsigned width, height;
+	unsigned error = lodepng::decode(image, width, height, filename);
+
+	// If there's an error, display it.
+	if (error != 0)
+	{
+		std::cout << "error " << error << ": " << lodepng_error_text(error) << std::endl;
+		return 1;
+	}
+
+	// Here the PNG is loaded in "image". All the rest of the code is SDL and OpenGL stuff.
+
+	int screenw = width;
+	if (screenw > 1024) screenw = 1024;
+	int screenh = height;
+	if (screenh > 768) screenw = 768;
+
+	// The official code for "Setting Your Raster Position to a Pixel Location" (i.e. set up a camera for 2D screen)
+	glViewport(0, 0, screenw, screenh);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0, screenw, screenh, 0, -1, 1);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	// Make some OpenGL properties better for 2D and enable alpha channel.
+	glDisable(GL_CULL_FACE);
+	glDisable(GL_DEPTH_TEST);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_BLEND);
+	glDisable(GL_ALPHA_TEST);
+
+	if (glGetError() != GL_NO_ERROR)
+	{
+		std::cout << "Error initing GL" << std::endl;
+		return 1;
+	}
+
+	// Texture size must be power of two for the primitive OpenGL version this is written for. Find next power of two.
+	size_t u2 = 1; while (u2 < width) u2 *= 2;
+	size_t v2 = 1; while (v2 < height) v2 *= 2;
+	// Ratio for power of two version compared to actual version, to render the non power of two image with proper size.
+	double u3 = (double)width / u2;
+	double v3 = (double)height / v2;
+
+	// Make power of two version of the image.
+	std::vector<unsigned char> image2(u2 * v2 * 4);
+	for (size_t y = 0; y < height; y++)
+		for (size_t x = 0; x < width; x++)
+			for (size_t c = 0; c < 4; c++)
+			{
+				image2[4 * u2 * y + 4 * x + c] = image[4 * width * y + 4 * x + c];
+			}
+
+	// Enable the texture for OpenGL.
+	glEnable(GL_TEXTURE_2D);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); //GL_NEAREST = no smoothing
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, 4, u2, v2, 0, GL_RGBA, GL_UNSIGNED_BYTE, &image2[0]);
+
+	glColor4ub(255, 255, 255, 255);
+
+	while (!start.closed()){
+	start.clear();
+
+	/*b_start.draw();
+	b_exit.draw();
+	if (b_start.clicked()){
+	start.~Window();
+	}
+	else if (b_exit.clicked()){
+	start.~Window();
+	exit = true;
+	}*/
+
+	// Draw the texture on a quad, using u3 and v3 to correct non power of two texture size.
+	glBegin(GL_QUADS);
+	glTexCoord2d(0, 0); glVertex2f(0, 0);
+	glTexCoord2d(u3, 0); glVertex2f(width, 0);
+	glTexCoord2d(u3, v3); glVertex2f(width, height);
+	glTexCoord2d(0, v3); glVertex2f(0, height);
+	glEnd();
+
+
+	start.update();
+	// Redraw and clear screen.
+	glClearColor(0, 0, 0, 0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	}
+}
